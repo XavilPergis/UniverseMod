d792b5d37bd363b8659116bad5eb17b83d603ace
Avi <4951693+XavilPergis@users.noreply.github.com>
2024-01-09 15:53:39 -0500

    improve look of stars

--- src/main/resources/assets/ultraviolet/shaders/main/celestial/star.glsl
+++ src/main/resources/assets/ultraviolet/shaders/main/celestial/star.glsl
@@ -15,30 +15,42 @@ const float STAR_REFERENCE_BRIGHTNESS = 1.0;
 const float STAR_MIN_BRIGHTNESS = 15.0;
 const float STAR_MAX_BRIGHTNESS = 25.0;
 
+const float NOISE_DETAIL_RIDGE_OFFSET = 0.035;
+
 float curveStarBrightness(in float brightness) {
 	float t = brightness / (brightness + STAR_REFERENCE_BRIGHTNESS);
 	return mix(STAR_MIN_BRIGHTNESS, STAR_MAX_BRIGHTNESS, t);
 }
 
-vec3 shadeCelestialObject(inout FragmentInfo frag, in Star star) {
+vec3 shadeCelestialObject(inout FragmentInfo frag, in Star node) {
 	// domain warped noise
-	float a = mix(-1f, 1f, noiseFbm(DEFAULT_FBM, frag.normalW, 0.0));
-	float b = mix(-1f, 1f, noiseFbm(DEFAULT_FBM, frag.normalW, 1.0));
-	vec3 offset = 0.8 * vec3(a, 0.0, b) + vec3(uTime / 1000);
-	float n = abs(mix(-1f, 1f, noiseFbm(DEFAULT_FBM, 2.0 * frag.normalW + offset)));
+	vec4 p = vec4(frag.normalW, float(node.seed % 100000) / 100000.0);
+
+	float a1 = mix(-1f, 1f, noiseFbm(FbmInfo(3, 1.0, 1.5, 0.4, 2.5), p + vec4(0.0, 0.0, 0.0, 1.0)));
+	float b1 = mix(-1f, 1f, noiseFbm(FbmInfo(3, 1.0, 1.5, 0.4, 2.5), p + vec4(0.0, 0.0, 0.0, 0.0)));
+	p.xyz += 1.6 * vec3(a1, 0.0, b1) + vec3(uTime / 1000000.0);
+
+	float a2 = mix(-1f, 1f, noiseFbm(FbmInfo(3, 1.0, 0.6, 0.7, 3.5), p + vec4(0.0, 0.0, 0.0, 1.0)));
+	float b2 = mix(-1f, 1f, noiseFbm(FbmInfo(3, 1.0, 0.6, 0.7, 3.5), p + vec4(0.0, 0.0, 0.0, 0.0)));
+	p.xyz += 0.5 * vec3(a2, 0.0, b2) + vec3(uTime / 100000.0);
+
+	float n = abs(mix(-1f, 1f, noiseFbm(FbmInfo(4, 1.0, 1.0, 0.7, 3.0), p)));
 
 	// shape noise into ridges
-	n = pow(n, 0.2);
-	n = 1.0 - n;
+	n *= 1.0 + NOISE_DETAIL_RIDGE_OFFSET;
+	n -= NOISE_DETAIL_RIDGE_OFFSET;
+	n = abs(n);
+	n = pow(n, 0.7);
+	n *= 2.0;
 
 	vec3 res = vec3(0.0);
 
 	// base emissive modulated by the noise field
-	res += curveStarBrightness(star.brightness) * star.color;
+	res += curveStarBrightness(node.brightness) * node.color;
 	res *= n;
 
 	// make the outer rim of the star look all shiny
-	res += 12.0 * star.color * fresnelFactor(frag.normalV, 1.0);
+	res += 50.0 * node.color * fresnelFactor(normalize(frag.posV), frag.normalV);
 
 	return res;
 }
